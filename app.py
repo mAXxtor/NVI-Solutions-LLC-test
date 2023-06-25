40ccaec89c6111dac7aaeafeeaaca7c4cea0b1bf
Max Verbitskiy <maxxtor@yandex.ru>
2023-06-25 23:03:02 +0400

    clear app coments

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import os
-from flask import Flask, redirect, request, render_template
+from flask import Flask, request, render_template
 from torchvision.transforms.functional import get_image_size
 from torchvision.io import read_image
 from torchvision.models import ResNet50_Weights, resnet50
 from werkzeug.utils import secure_filename
-DEBUG = True
-# папка для сохранения загруженных файлов
+# Папка для сохранения загруженных файлов
 UPLOAD_FOLDER = './test_img/'
-# расширения файлов, которые разрешено загружать
+# Расширения файлов, которые разрешено загружать
 ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
@@ -17,16 +16,21 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
 
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     pic_data = {}
     if request.method == 'POST':
         file = request.files['file']
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'],
-                               secure_filename(file.filename)))
+        if file and allowed_file(file.filename):
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'],
+                      secure_filename(file.filename)))
         img = read_image(f'test_img/{file.filename}')
         width, height = get_image_size(img)
-
 
         # Step 1: Initialize model with the best available weights
         weights = ResNet50_Weights.DEFAULT
@@ -42,10 +46,7 @@ def index():
         # Step 4: Use the model and print the predicted category
         prediction = model(batch).squeeze(0).softmax(0)
         class_id = prediction.argmax().item()
-        score = prediction[class_id].item()
-        category_name = weights.meta["categories"][class_id]
-        print(f"{category_name}: {100 * score:.1f}%")
-        print(f"{width}, {height}")
+        category_name = weights.meta['categories'][class_id]
+
         pic_data = dict(category=category_name, width=width, height=height)
-        # return redirect(request.url)
     return render_template('index.html', **pic_data)
